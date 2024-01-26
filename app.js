55142c75989e5a3ca365ee67984f1cd2a7c7380c
mdsajid3037 <114169937+mdsajid3037@users.noreply.github.com>
2024-01-26 20:59:54 +0000

    Again commit at all

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Mohammad Sajid")
+function rest(){
+    let n =  prompt("Enter the number");
+    if(n >18 && n<60){
+        console.log("you are eligible for voting")
+    }
+    else if(n<18){
+        console.log("you are not eligible for voting")
+    }
+    else{
+        console.log("you are senior citizen,so you are also eligible for voting")
+    }
+}
